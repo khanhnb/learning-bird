329a381f41c5cd5474d3dcf744d5d7dad4e93997
ck01 <clarkkent.k41@gmail.com>
2018-07-02 01:20:39 +0700

    flappybird unity

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public delegate void GameDelegate();
+	public static event GameDelegate OnGameStarted;
+	public static event GameDelegate OnGameOverConfirmed;
+
+	public static GameManager Instance;
+	public GameObject startPage;
+	public GameObject gameOverPage;
+	public GameObject countdownPage;
+	public Text scoreText;
+
+	enum PageState
+	{
+		None,
+		Start,
+		GameOver,
+		Countdown
+	}
+	int score = 0;
+	bool gameOver = false;
+    public bool GameOver { get { return gameOver; } }
+
+	void Awake(){
+		Instance = this;
+	}
+
+	void SetPageState(PageState state){
+		switch (state)
+		{
+			case PageState.None:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countdownPage.SetActive(false);
+				break;
+			case PageState.Start:
+				startPage.SetActive(true);
+				gameOverPage.SetActive(false);
+				countdownPage.SetActive(false);
+			break;
+			case PageState.GameOver:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(true);
+				countdownPage.SetActive(false);
+			break;
+			case PageState.Countdown:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countdownPage.SetActive(true);
+			break;
+		}
+	}
+
+	public void StartGame(){
+
+	}
+
+	public void ConfirmGameOver(){
+	
+	}
+}
